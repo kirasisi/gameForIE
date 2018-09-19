3304c66df7b5a3969431671295185e86ad312cbd
kirasisi <wenfei.jiang@yahoo.com>
2018-09-19 17:13:12 +1000

    tree appear with user touch

---
+++ Assets/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public GameObject tree;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            if (touch.phase == TouchPhase.Began)
+                Instantiate(tree, touchPos, Quaternion.identity);
+        }
+	}
+}
